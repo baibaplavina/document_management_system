7462db52f021cb3554b3acf0f505d1352275f69e
Baiba Plavina <BPlavina@gmail.com>
2023-10-09 13:49:42 +0300

    09102023

--- src/main/java/com/example/documentmanagement/WebsiteController.java
+++ src/main/java/com/example/documentmanagement/WebsiteController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -27,7 +27,7 @@ public class WebsiteController {
     }
     @GetMapping("/create-administrator")
     public String displayCreateAdminPage(){
-        return "administratorProfile";
+        return "createAdministrator";
     }
 
 
